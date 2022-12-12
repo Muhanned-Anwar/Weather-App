828c5ac71faea416d32989d7900793b3b56132e2
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2022-12-12 19:03:14 +0200

    Add AppId to constances.dart

--- lib/core/utils/constances.dart
+++ lib/core/utils/constances.dart
@@ -1,3 +1,4 @@
 class AppConstance{
-  static const String baseUrl = "https://api.openweathermap.org/data/2.5";
+  static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
+  static const String appId = '347e6ae23e75ba25ea60276f7289c226';
 }
