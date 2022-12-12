b4ae19f479d4ed7228d2c5a6022beda8e1e245f3
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2022-12-12 21:25:08 +0200

    Import data in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:weather/weather/data/datasource/base_remote_data_source.dart';
+import 'package:weather/weather/data/repository/weather_repository.dart';
+import 'package:weather/weather/domain/entities/weather.dart';
+import 'package:weather/weather/domain/repository/base_weather_repository.dart';
+import 'package:weather/weather/domain/usecases/get_weather_by_country_name.dart';
 
-void main() {
+Weather? weather;
+void main() async{
+  BaseRemoteDataSource baseRemoteDataSource = RemoteDataSource();
+  BaseWeatherRepository baseWeatherRepository = WeatherRepository(baseRemoteDataSource);
+  Weather weather = await GetWeatherByCountryName(baseWeatherRepository).execute('Gaza');
+  print('${weather.cityName} + ${weather.description} + ${weather.id}');
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Container(),
+    );
   }
 }
